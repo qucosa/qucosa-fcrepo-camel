538af77d1bfee9879ac9ffcae75ed29aed45d9b3
dseelig <daniel.seelig@slub-dresden.de>
2018-04-17 12:43:26 +0200

    Add default home dir property to web xml
    - Use home dir default property in application context xml.
    - Remove configuration test.

--- src/main/java/de/qucosa/routes/Main.java
+++ src/main/java/de/qucosa/routes/Main.java
@@ -25,7 +25,7 @@ public class Main extends RouteBuilder {
 
     @PropertyInject("update.delay")
     long updateDelay;
-
+    
     @Override
     public void configure() {
 //        from("direct:oaiprovider")
@@ -38,7 +38,7 @@ public class Main extends RouteBuilder {
                 .id("update-message-route")
                 .resequence(body()).timeout(TimeUnit.SECONDS.toMillis(updateDelay))
                 .log("Perform updates for ${body}");
-//                .to("fcrepo:fedora:METS?shema=http&host=${fedora.host}&port=${fedora.port}")
+//                .to("fcrepo:fedora:METS?schema=${fedora.schema}&host=${fedora.host}&port=${fedora.port}")
 //                .to("direct:oaiprovider");
 
 

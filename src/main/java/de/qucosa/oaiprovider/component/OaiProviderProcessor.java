590c19f1f46d5536f215b6f4e77e6fe455cd4745
dseelig <daniel.seelig@slub-dresden.de>
2018-05-02 15:02:13 +0200

    Remove all transform properties and methods from processor.

--- src/main/java/de/qucosa/oaiprovider/component/OaiProviderProcessor.java
+++ src/main/java/de/qucosa/oaiprovider/component/OaiProviderProcessor.java
@@ -16,70 +16,14 @@
 
 package de.qucosa.oaiprovider.component;
 
-import de.qucosa.fcrepo3.component.mapper.MetsXmlMapper;
-import de.qucosa.oaiprovider.component.model.DissTerms;
-import de.qucosa.oaiprovider.component.model.RecordTransport;
-import de.qucosa.oaiprovider.component.model.SetsConfig;
-import de.qucosa.transformers.XMetaDissTransformer;
-import de.qucosa.utils.DateTimeConverter;
-import de.qucosa.utils.DocumentXmlUtils;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-
-import javax.xml.transform.stream.StreamSource;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpressionException;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 public class OaiProviderProcessor implements Processor {
-    private static final String RECORD_TEMPLATE_FILE = "oaiprovider/record.xml";
-    private RecordTransport xmetadiss = new RecordTransport();
-    private RecordTransport epicur = new RecordTransport();
-    private Set<RecordTransport> disseminations = new HashSet<RecordTransport>();
-    private DissTerms dt = null;
-    private SetsConfig sets = null;
-    private MetsXmlMapper metsXml = null;
-
-    public OaiProviderProcessor() {
-        this.dt = new DissTerms();
-        this.sets = new SetsConfig();
-    }
 
     @Override
     public void process(Exchange exchange) throws Exception {
-        Document metsDoc = (Document) exchange.getIn().getBody();
-        metsXml = new MetsXmlMapper(metsDoc, dt.getMapXmlNamespaces());
 
-        //TODO extract transformations to camel route
-        buildXMetaDissplusObject(metsDoc, exchange);
-        disseminations.add(xmetadiss);
-
-//        buildEpicurObject(metsDoc);
-        disseminations.add(epicur);
-
-        exchange.getIn().setBody(disseminations);
-    }
-
-    private RecordTransport buildXMetaDissplusObject(Document metsDoc, Exchange exchange) throws Exception {
-        XMetaDissTransformer transformer = new XMetaDissTransformer((String) exchange.getProperty("transfer.url.pattern"), "", true);
-        Document result = transformer.transformXmetaDissplus(metsDoc,
-                new StreamSource(getClass().getClassLoader().getResource("xslt/mets2xmetadissplus.xsl").getPath()));
-        XPath xPath = DocumentXmlUtils.xpath(dt.getMapXmlNamespaces());
-
-        xmetadiss.setPid(metsXml.pid());
-        xmetadiss.setModified(DateTimeConverter.timestampWithTimezone(metsXml.lastModDate()));
-        xmetadiss.setPrefix("xmetadissplus");
-        xmetadiss.setData(result);
-        xmetadiss.setOaiId("");
-        xmetadiss.setSets(getSetSpecs("xmetadissplus", result));
-
-        return xmetadiss;
     }
 
 /*
@@ -99,53 +43,4 @@ public class OaiProviderProcessor implements Processor {
         return epicur;
     }
 */
-    private List<String> getSetSpecs(String format, Document dissemination) throws XPathExpressionException {
-        List<String> setSpecs = new ArrayList<>();
-
-        for (SetsConfig.Set setObj : sets.getSetObjects()) {
-            String predicateKey = null;
-            String predicateValue = null;
-
-            if (setObj.getPredicate() != null && !setObj.getPredicate().isEmpty()) {
-
-                if (setObj.getPredicate().contains("=")) {
-                    String[] predicate = setObj.getPredicate().split("=");
-                    predicateKey = predicate[0];
-                    predicateValue = predicate[1];
-
-                    if (!predicateValue.contains("/")) {
-
-                        if (matchTerm(predicateKey, predicateValue, format, dissemination)) {
-                            setSpecs.add(setObj.getSetSpec());
-                        }
-                    } else {
-                        String[] predicateValues = predicateValue.split("/");
-
-                        if (predicateValues.length > 0) {
-
-                        }
-                    }
-                } else {
-                    predicateKey = setObj.getPredicate();
-                }
-            }
-        }
-
-        return setSpecs;
-    }
-
-    private boolean matchTerm(String key, String value, String format, Document dissemination) throws XPathExpressionException {
-        DissTerms.Term term = dt.getTerm(key, format);
-        XPath xPath = DocumentXmlUtils.xpath(dt.getMapXmlNamespaces());
-        Node node = null;
-
-        if (term != null) {
-
-            if (!term.getTerm().isEmpty()) {
-                node = (Node) xPath.compile(term.getTerm().replace("$val", value)).evaluate(dissemination, XPathConstants.NODE);
-            }
-        }
-
-        return (node != null);
-    }
 }

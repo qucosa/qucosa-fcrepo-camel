8ab34783bb58b1377696ab1c9905b4724992c332
dseelig <daniel.seelig@slub-dresden.de>
2018-05-09 08:12:25 +0200

    Add uri param properties and methods. Add date validator mathod, for validate from and / or until uri param. Add switch clause for multiple consumers. Add unsuported exception for producer.

--- src/main/java/de/qucosa/component/oaipmh/OaiPmhEndpoint.java
+++ src/main/java/de/qucosa/component/oaipmh/OaiPmhEndpoint.java
@@ -16,24 +16,121 @@
 
 package de.qucosa.component.oaipmh;
 
+import de.qucosa.component.oaipmh.model.IdentifiersPollConsumer;
+import org.apache.camel.Component;
 import org.apache.camel.Consumer;
 import org.apache.camel.Processor;
 import org.apache.camel.Producer;
 import org.apache.camel.impl.DefaultEndpoint;
+import org.apache.camel.impl.ScheduledPollConsumer;
+import org.apache.camel.spi.UriParam;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class OaiPmhEndpoint extends DefaultEndpoint {
+    @UriParam
+    private String verb = "ListIdentifiers";
+
+    @UriParam
+    private String from;
+
+    @UriParam
+    private String until;
+
+    @UriParam
+    private String set;
+
+    @UriParam
+    private String metadataPrefix = "oai_dc";
+
+    public OaiPmhEndpoint(String uri, Component component, String resumptionToken) {
+        super(uri, component);
+    }
+
     @Override
     public Producer createProducer() throws Exception {
-        return null;
+        throw new UnsupportedOperationException("The oai pmh endpoint is not an producer endpoint.");
     }
 
     @Override
     public Consumer createConsumer(Processor processor) throws Exception {
-        return null;
+        Consumer consumer = null;
+
+        switch (getVerb().toLowerCase()) {
+            default:
+                consumer = new IdentifiersPollConsumer(this, processor);
+        }
+
+        validateDateParameters();
+
+        return consumer;
     }
 
     @Override
     public boolean isSingleton() {
         return false;
+    }
+
+    public String getVerb() {
+        return verb;
+    }
+
+    public void setVerb(String verb) {
+        this.verb = verb;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getUntil() {
+        return until;
+    }
+
+    public void setUntil(String until) {
+        this.until = until;
+    }
+
+    public String getSet() {
+        return set;
+    }
+
+    public void setSet(String set) {
+        this.set = set;
+    }
+
+    public String getMetadataPrefix() {
+        return metadataPrefix;
+    }
+
+    public void setMetadataPrefix(String metadataPrefix) {
+        this.metadataPrefix = metadataPrefix;
+    }
+
+    private void validateDateParameters() throws ParseException {
+        DateFormat df = new SimpleDateFormat("yyyy-mm-dd");
+        df.setLenient(false);
+
+        if (from != null && !from.isEmpty()) {
+            try {
+                df.parse(from);
+            } catch (ParseException e) {
+                throw new ParseException("The from parameter has wrong date format.", 1);
+            }
+        }
+
+        if (until != null && !until.isEmpty()) {
+            try {
+                df.parse(until);
+            } catch (ParseException e) {
+                throw new ParseException("The until parameter has wrong date format.", 1);
+            }
+        }
     }
 }

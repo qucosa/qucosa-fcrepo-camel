85ac3837528e666bc05155efe36891359428408f
dseelig <daniel.seelig@slub-dresden.de>
2018-05-15 13:28:33 +0200

    Add list contains object check.

--- src/main/java/de/qucosa/component/fcrepo3/aggregate/RecordListAggregator.java
+++ src/main/java/de/qucosa/component/fcrepo3/aggregate/RecordListAggregator.java
@@ -57,7 +57,10 @@ public class RecordListAggregator implements AggregationStrategy {
         }
 
         if (eOld.getIn().getBody() instanceof RecordTransport && eNew.getIn().getBody() instanceof List) {
-            ((List) eNew.getIn().getBody()).add(eOld.getIn().getBody(RecordTransport.class));
+
+            if (!((List) eNew.getIn().getBody()).contains(eOld.getIn().getBody(RecordTransport.class))) {
+                ((List) eNew.getIn().getBody()).add(eOld.getIn().getBody(RecordTransport.class));
+            }
         }
 
         if (eOld.getIn().getBody() instanceof List && eNew.getIn().getBody() instanceof List) {

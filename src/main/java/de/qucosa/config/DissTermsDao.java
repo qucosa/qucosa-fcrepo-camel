800c2079ba96b032571386087fdd9f75844100fe
dseelig <daniel.seelig@slub-dresden.de>
2018-06-22 08:19:08 +0200

    Im plements logic and mapping.

--- src/main/java/de/qucosa/config/DissTermsDao.java
+++ src/main/java/de/qucosa/config/DissTermsDao.java
@@ -16,5 +16,119 @@
 
 package de.qucosa.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.qucosa.model.DissTerms;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class DissTermsDao {
+
+    private Logger logger = LoggerFactory.getLogger(DissTerms.class);
+
+    private InputStream config;
+
+    private DissTermsMapper mapping;
+
+    public DissTermsDao(String path) throws FileNotFoundException {
+        this(new File(path));
+    }
+
+    public DissTermsDao(File file) throws FileNotFoundException {
+        this((InputStream) new FileInputStream(file));
+    }
+
+    public DissTermsDao(InputStream stream) {
+        this.config = stream;
+
+        ObjectMapper om = new ObjectMapper();
+
+        try {
+            mapping = om.readValue(config, DissTermsMapper.class);
+        } catch (IOException e) {
+            logger.error("Cannot parse dissemination-conf JSON file.");
+        }
+    }
+
+    public Map<String, String> getMapXmlNamespaces() {
+        HashSet<DissTerms.XmlNamspace> xmlNamespaces = (HashSet<DissTerms.XmlNamspace>) mapping.getXmlnamespaces();
+        Map<String, String> map = new HashMap<>();
+
+        for (DissTerms.XmlNamspace namspace : xmlNamespaces) {
+            map.put(namspace.getPrefix(), namspace.getUrl());
+        }
+
+        return map;
+    }
+
+    public Set<DissTerms.XmlNamspace> getSetXmlNamespaces() { return mapping.getXmlnamespaces(); }
+
+    public DissTerms.XmlNamspace getXmlNamespace(String prefix) {
+        DissTerms.XmlNamspace xmlNamspace = null;
+
+        for (DissTerms.XmlNamspace namespace : mapping.getXmlnamespaces()) {
+
+            if (namespace.getPrefix().equals(prefix)) {
+                xmlNamspace = namespace;
+            }
+        }
+
+        return xmlNamspace;
+    }
+
+    public DissTerms.Term getTerm(String diss, String name) {
+        HashSet<DissTerms.DissTerm> dissTerms = (HashSet<DissTerms.DissTerm>) mapping.getDissTerms();
+        DissTerms.Term term = null;
+
+        for (DissTerms.DissTerm dt : dissTerms) {
+
+            if (!dt.getDiss().equals(diss)) {
+                logger.error(diss + " is does not exists in dissemination-config.");
+                continue;
+            }
+
+            if (dt.getTerms().isEmpty()) {
+                logger.error(diss + " has no terms config.");
+                continue;
+            }
+
+            for (DissTerms.Term t : dt.getTerms()) {
+
+                if (!t.getName().equals(name)) {
+                    logger.error("The term name " + name + " is not available in dissemination " + diss);
+                    continue;
+                }
+
+                term = t;
+            }
+
+            if (term != null) {
+                break;
+            }
+        }
+
+        return term;
+    }
+
+    public Set<DissTerms.DissFormat> getFormats() { return mapping.getFormats(); }
+
+    public DissTerms.DissFormat getFormat(String format) {
+        DissTerms.DissFormat dissFormat = null;
+
+        for (DissTerms.DissFormat df : mapping.getFormats()) {
+
+            if (df.getMdprefix().equals(format)) {
+                dissFormat = df;
+                break;
+            }
+        }
+
+        return dissFormat;
+    }
+
 }
--- src/main/java/de/qucosa/config/DissTermsMapper.java
+++ src/main/java/de/qucosa/config/DissTermsMapper.java
@@ -16,5 +16,110 @@
 
 package de.qucosa.config;
 
-public class DissTermsMapper {
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import de.qucosa.model.DissTerms;
+
+import java.io.Serializable;
+import java.util.Set;
+
+@JsonAutoDetect
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class DissTermsMapper implements Serializable {
+    @JsonProperty("xmlnamespaces")
+    private Set<DissTerms.XmlNamspace> xmlnamespaces;
+
+    @JsonProperty("dissTerms")
+    private Set<DissTerms.DissTerm> dissTerms;
+
+    @JsonProperty("formats")
+    private Set<DissTerms.DissFormat> formats;
+
+    public Set<DissTerms.XmlNamspace> getXmlnamespaces() { return xmlnamespaces; }
+
+    public void setXmlnamespaces(Set<DissTerms.XmlNamspace> xmlnamespaces) { this.xmlnamespaces = xmlnamespaces; }
+
+    public Set<DissTerms.DissTerm> getDissTerms() { return dissTerms; }
+
+    public void setDissTerms(Set<DissTerms.DissTerm> dissTerms) { this.dissTerms = dissTerms; }
+
+    public Set<DissTerms.DissFormat> getFormats() { return formats; }
+
+    public void setFormats(Set<DissTerms.DissFormat> formats) { this.formats = formats; }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class XmlNamspace {
+        @JsonProperty("prefix")
+        private String prefix;
+
+        @JsonProperty("url")
+        private String url;
+
+        public String getPrefix() { return prefix; }
+
+        public void setPrefix(String prefix) { this.prefix = prefix; }
+
+        public String getUrl() { return url; }
+
+        public void setUrl(String url) { this.url = url; }
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class DissTerm {
+        @JsonProperty("diss")
+        private String diss;
+
+        @JsonProperty("terms")
+        private Set<DissTerms.Term> terms;
+
+        public String getDiss() { return diss; }
+
+        public void setDiss(String diss) { this.diss = diss; }
+
+        public Set<DissTerms.Term> getTerms() { return terms; }
+
+        public void setTerms(Set<DissTerms.Term> terms) { this.terms = terms; }
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Term {
+        @JsonProperty("name")
+        private String name;
+
+        @JsonProperty("term")
+        private String term;
+
+        public String getName() { return name; }
+
+        public void setName(String name) { this.name = name; }
+
+        public String getTerm() { return term; }
+
+        public void setTerm(String term) { this.term = term; }
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class DissFormat {
+        @JsonProperty("mdprefix")
+        private String mdprefix;
+
+        @JsonProperty("schemaurl")
+        private String schemaUrl;
+
+        @JsonProperty("namespace")
+        private String namespace;
+
+        public String getMdprefix() { return mdprefix; }
+
+        public void setMdprefix(String mdprefix) { this.mdprefix = mdprefix; }
+
+        public String getSchemaUrl() { return schemaUrl; }
+
+        public void setSchemaUrl(String schemaUrl) { this.schemaUrl = schemaUrl; }
+
+        public String getNamespace() { return namespace; }
+
+        public void setNamespace(String namespace) { this.namespace = namespace; }
+    }
 }

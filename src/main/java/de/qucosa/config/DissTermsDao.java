027c6cc94ad380f1cf4b9b557c0f93d58aed19f1
dseelig <daniel.seelig@slub-dresden.de>
2018-06-22 10:42:11 +0200

    Optimize imports.

--- src/main/java/de/qucosa/config/DissTermsDao.java
+++ src/main/java/de/qucosa/config/DissTermsDao.java
@@ -20,7 +20,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
